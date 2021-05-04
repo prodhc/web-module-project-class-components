3e7375a2a5eadccad8df0799dc2f46ac97a6dde0
prodhc <zionywood@gmail.com>
2021-05-03 17:40:50 -0700

    MVP

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './components/Todo.scss'
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
